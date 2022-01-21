d3475655d7e0e6e52008a5b9e5402ac68efe946c
edson-dantass <79416133+edson-dantass@users.noreply.github.com>
2022-01-21 09:56:04 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent, useRef } from "react";
 
 function App() {
+  const formRef = useRef(null);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (formRef.current) {
+      const formData = new FormData(formRef.current);
+      const data = Object.fromEntries(formData);
+      console.log(data);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form ref={formRef} onSubmit={handleSubmit}>
+        <div>
+          <label>Nome</label>
+          <input type="text" name="nome" />
+        </div>
+        <div>
+          <label>Data</label>
+          <input type="date" name="data" />
+        </div>
+        <div>
+          <label>Sexo</label>
+          <input type="radio" name="sexo" value="masculino" />
+          <input type="radio" name="sexo" value="feminino" />
+        </div>
+        <div>
+          <label>Perfil foto</label>
+          <input type="file" name="perfil" />
+        </div>
+        <div>
+          <label>Idade</label>
+          <input type="number" name="idade" />
+        </div>
+        <div>
+          <label>Idade</label>
+          <input type="range" name="idade2" />
+        </div>
+        <div>
+          <label>Cor favorita</label>
+          <select name="cor" defaultValue="">
+            <option selected disabled value="">
+              Selecione
+            </option>
+            <option value="vermelho">Vermelho</option>
+            <option value="azul">Azul</option>
+          </select>
+        </div>
+        <div>
+          <label>Checkbox</label>
+          <input type="checkbox" name="check" value="check 1" />
+          <input type="checkbox" name="check" value="check 2" />
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   );
 }
